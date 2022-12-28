140fbc89ab7a5298939acb5df013becbc231ec39
hauk atkinson <haukatkinson@aol.com>
2022-12-28 11:37:01 -0600

    Adding and testing test.py

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Callculator functions
+"""
+
+def add(x,y):
+    """Add x and y and return reuslt."""
+    return x + y
+
+def subtract(x,y):
+    """Subtract x from y and return result."""
+    return y - x
