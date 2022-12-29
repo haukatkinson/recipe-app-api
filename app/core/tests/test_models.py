980d47a62bf9a58325afb62519e1992f29644872
hauk atkinson <haukatkinson@aol.com>
2022-12-29 12:16:45 -0600

    added functions to test and create super_users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,4 +35,14 @@ class ModelTests(TestCase):
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without and email raises a ValueError"""
         with self.assertRaises(ValueError):
-            get_user_model().object.create_user('', 'test123')
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
