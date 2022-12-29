be3489690231cbcb2818864cbc0b13c515c83f62
hauk atkinson <haukatkinson@aol.com>
2022-12-29 10:58:37 -0600

    implemented UserManager

--- app/core/models.py
+++ app/core/models.py
@@ -9,11 +9,25 @@ from django.contrib.auth.models import (
 )
 
 
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_feild):
+        """Create, save and return and new user."""
+        user = self.model(email=email, **extra_feilds)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+
+    object= UserManager()
 
     USERNAME_FEILD = 'email'
