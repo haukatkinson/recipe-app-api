667fda62d80334a3d6bc8fa810b6c666318a8e27
hauk atkinson <haukatkinson@aol.com>
2022-12-29 11:59:52 -0600

    added user manager

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -23,11 +23,16 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
-            ['test1@EXAMPLE.com', 'test1@example.com']
-            ['Test2@Example.com', 'Test2@example.com']
-            ['TEST3@EXAMPLE.com', 'TEST3@example.com']
-            ['test4@example.com', 'test4@example.com']
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.com', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without and email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().object.create_user('', 'test123')
