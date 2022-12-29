89f56441ffcd8841f8503c217ff65e70fe8f36d1
hauk atkinson <haukatkinson@aol.com>
2022-12-29 15:51:23 -0600

    Implemented user page and  test

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -39,3 +39,10 @@ class AdminSiteTest(TestCase):
 
         self.assertEqual(res.status_code, 200)
 
+    def test_edit_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
