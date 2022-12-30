ce68ca184344de9c80bb5e39e4b886225c854e49
hauk atkinson <haukatkinson@aol.com>
2022-12-30 15:43:18 -0600

    fixing typos in test

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,7 +11,7 @@ from rest_framework import status
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 
-def create_user (**params):
+def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
 
@@ -57,23 +57,23 @@ class PublicApiTest(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exsists = get_user_model().objects.filter(
+        user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
-        self.assertFalse(user_exsists)
+        self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
         """Test generates token for valid credentials."""
         user_details = {
             'name': 'Test Name',
-            'email': 'test2@example.com',
+            'email': 'test@example.com',
             'password': 'test-user-password123',
         }
         create_user(**user_details)
 
         payload = {
             'email': user_details['email'],
-            'password':user_details['password'],
+            'password': user_details['password'],
         }
         res = self.client.post(TOKEN_URL, payload)
 
@@ -84,7 +84,7 @@ class PublicApiTest(TestCase):
         """Test returns error if credentials invalid."""
         create_user(email='test@example.com', password='goodpass')
 
-        payload = {'email': 'test@example','password': 'badpass'}
+        payload = {'email': 'test@example.com','password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
@@ -92,8 +92,8 @@ class PublicApiTest(TestCase):
 
     def test_create_token_email_not_found(self):
         """Test error returned if user not found for given email."""
-        payload = {'email': 'test@example', 'password': 'pass123'}
-        res= self.client.post(TOKEN_URL, payload)
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
