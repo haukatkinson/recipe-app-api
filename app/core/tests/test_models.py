07dad4a81d2451ed91b38f30149b997f5e974298
hauk atkinson <haukatkinson@aol.com>
2023-01-02 15:33:34 -0600

    fixed typo in test_commands

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -51,7 +51,7 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
-    def test_vreate_recipe(self):
+    def test_create_recipe(self):
         """Test creating a recipe is successful."""
         user = get_user_model().objects.create_user(
             'test@example.com',
